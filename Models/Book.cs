7d6a9745e5f0820f753e3167e44a20ca09de30ed
Brian White <brian@thirdember.com>
2024-05-04 16:43:54 -0400

    initial working table

---
+++ Models/Book.cs
@@ -0,0 +1,39 @@
+namespace alexandria.web.Models
+{
+    public class Author
+    {
+        public long? Id { get; set; }
+        public string? Name { get; set; }
+    }
+
+    public class Book
+    {
+        public long Id { get; set; }
+        public string? Title { get; set; }
+        public string? Authors { get; set; }
+        public List<Author>? AuthorList { get; set; }
+        public string? SeriesInfo { get; set; }
+        public long? SeriesId { get; set; }
+        public DateTime? DateAdded { get; set; }
+        public DateTime? PublicationDate { get; set; }
+        public string? MobiFullPath { get; set; }
+        public string? Azw3FullPath { get; set; }
+        public string? EpubFullPath { get; set; }
+
+        public Book()
+        {
+            Id = 0;
+            Title = "No Book Found";
+            Authors = "Unknown";
+
+            SeriesInfo = null;
+
+            DateAdded = null;
+            PublicationDate = null;
+
+            MobiFullPath = null;
+            Azw3FullPath = null;
+            EpubFullPath = null;
+        }
+    }
+}
