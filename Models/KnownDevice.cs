0dc11fde4f94897ab1d6d3096918acfea606414e
Brian White <brian@thirdember.com>
2024-05-14 18:56:00 -0400

    change to mudtable from grid, use separate edit view instead of inline

--- Models/KnownDevice.cs
+++ Models/KnownDevice.cs
@@ -1,12 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace alexandria.web.Models
 {
     public class KnownDevice
     {
-        public long? Id { get; set; }
-        public string? DeviceName { get; set; }
-        public string? EbookDirectory { get; set; }
-        public string? SavePathTemplate { get; set; }
-        public string? Vendor { get; set; }
-        public string? SerialNumber { get; set; }
+        [Required]
+        public long Id { get; set; }
+
+        [Required]
+        public string DeviceName { get; set; }
+
+        [Required]
+        public string EbookDirectory { get; set; }
+
+        [Required]
+        public string SavePathTemplate { get; set; }
+
+        [Required]
+        public string Vendor { get; set; }
+
+        [Required]
+        public string SerialNumber { get; set; }
     }
 }
